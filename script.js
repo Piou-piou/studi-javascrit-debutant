14dbee6a3ff1606c2b6b9ac569691291e5727eb3
Piou-piou <pilloud.anthony@gmail.com>
2024-09-02 19:03:32 +0200

    feat: fisrt live

---
+++ script.js
@@ -0,0 +1,17 @@
+
+console.log(document);
+
+const prenom = document.getElementById('prenom');
+const nom = document.querySelector('#nom');
+
+console.log(prenom.value);
+console.log(prenom.id);
+console.log(prenom.type);
+
+console.log(nom.value);
+console.log(nom.id);
+console.log(nom.type);
+
+
+
+prenom.classList.toggle('test-class');
